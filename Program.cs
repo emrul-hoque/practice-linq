4ad988b37910f42c176651ccf57c89fda4ddedb9
emrul <emrul.hoque@gmail.com>
2022-08-30 16:42:28 -0500

    setup project and added a small example

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exam scores
+            int[] scores = { 97, 92, 81, 75, 60, 50 };
+
+            // How many got above 80
+
+            /*var studentsAboveEighty =   from score in scores
+                                where score > 80
+                                select score; */
+
+            var studentsAboveEighty = scores.Where(s => s > 80);
+
+            Console.WriteLine($"Total {studentsAboveEighty.Count()} student(s) got above 80.\n");
+
+            foreach(var score in studentsAboveEighty)
+            {
+                 Console.WriteLine(">>> Score: " + score);   
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# practice-linq
+# Language Integrated Query (LINQ)
+
+
+---
+## Links:
+- [Language Integrated Query (LINQ) (C#)](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/)
